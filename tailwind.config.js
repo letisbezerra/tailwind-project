dc4f6e914a9178e5eced3cce392de3da1d3ac5eb
letisbezerra <leticiabezerrauy@gmail.com>
2023-06-20 12:48:48 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+        100: '#c542f5',
+        200: '#9e35c4',
+        300: '#6d2487',
+        400: '#331040',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
